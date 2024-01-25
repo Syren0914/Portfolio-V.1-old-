688b1802c6cee0d62244e0c6613321140473ab7c
Kira <78575847+Syren0914@users.noreply.github.com>
2024-01-25 13:25:52 -0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+'use strict';
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+
+
+
