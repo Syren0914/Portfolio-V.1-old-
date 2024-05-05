081ed3b25e21ba6ee6f017b88578a9500a3a5b18
Kira <78575847+Syren0914@users.noreply.github.com>
2024-05-05 07:22:56 -0400

    update

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ function toggleMenu(){
 }
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var typingEffect = new Typed(".typedText", {
+      strings: ["Designer", "Youtuber", "Developer"],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000
+    });
+  });
 
 
